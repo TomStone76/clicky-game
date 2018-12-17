60eaa9db75b917fa82caf74231467d145d27352d
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-16 22:36:49 -0500

    added game info card

--- clicky-game/src/components/Click.js
+++ clicky-game/src/components/Click.js
@@ -2,6 +2,15 @@ import React from "react";
 
 const Click = () => (
     <div className="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+
+        <div className="card mb-4">
+            <div className="card-header">
+                <h3>Title Here</h3>
+            </div>
+            <div className="card-body">
+                <p className="card-text">Game information here</p>
+            </div>
+        </div>
     </div>
 )
