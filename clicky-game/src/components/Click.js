6de2533e23bb531e45ef216cc9326813a8f344a4
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-17 23:07:16 -0500

    Click.js mods

--- clicky-game/src/components/Click.js
+++ clicky-game/src/components/Click.js
@@ -2,20 +2,24 @@
 
 import React from "react";
 
-const Click = () => (
-    <div className="header">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+// const Click = () => (
+//     <div className="header">
+//             <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
 
-        <div className="card mb-4">
-            <div className="card-header">
-                <h3>Title Here</h3>
-            </div>
-            <div className="card-body">
-                <p className="card-text">Game information here</p>
-            </div>
-        </div>
-    </div>
-)
+//         <div className="card mb-4">
+//             <div className="card-header">
+//                 <h3>Title Here</h3>
+//             </div>
+//             <div className="card-body">
+//                 <p className="card-text">Game information here</p>
+//             </div>
+//         </div>
+//     </div>
+// )
+
+class Game extends React.Component {
+    
+}
 
 // NOTES:
 //-shuffle array
